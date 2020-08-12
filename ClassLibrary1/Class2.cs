cef0fa3328adbbf49b2942c4c4878edd1e5a5744
Sihan Cao <sihancao496@gmail.com>
2020-08-12 14:33:36 -0700

    add the code of family, family instance and family object

--- ClassLibrary1/Class2.cs
+++ ClassLibrary1/Class2.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
 
 namespace ClassLibrary1
 {
-    class Class2
+    [Transaction(TransactionMode.Manual)]
+    class Class2 : IExternalCommand
     {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+
+            return Result.Succeeded;
+        }
     }
 }
