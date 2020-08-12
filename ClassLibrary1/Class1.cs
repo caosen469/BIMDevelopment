e95f5890d95c6b9c286fc13b47e86a1ce857e8e2
Sihan Cao <sihancao496@gmail.com>
2020-08-11 23:06:10 -0700

    initiate the demo case

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            //界面交互的doc
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            //实际内容的doc
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            //[1]创建收集器
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+
+            return Result.Succeeded;
+
+        }
+    }
+}
