f84d68da647ddb4a3bac8d51310fa21a19b08029
Sihan Cao <sihancao496@gmail.com>
2020-08-18 20:40:17 -0700

    how to create a wall in a transcation

---
+++ ClassLibrary1/CreateWallDemo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.Revit;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ClassLibrary1
+{
+    [Transaction(TransactionMode.Manual)]
+    public class CreateWallDemo : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // [1] 获取当前文档
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            // [2] 获取 CW 102-50-100p 类型的墙
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            Element ele = collector.OfCategory(BuiltInCategory.OST_Walls).OfClass(typeof(WallType)).FirstOrDefault(x => x.Name == "CW 102-50-100P");
+
+            // [3] class transfer
+            WallType wallType = ele as WallType;
+
+            // [4] obtain the level
+            Level level = new FilteredElementCollector(doc).OfClass(typeof(Level)).FirstOrDefault(x => x.Name == "标高 1") as Level;
+
+            // [5] create control line
+            XYZ start = new XYZ(0, 0, 0);
+            XYZ end = new XYZ(10, 10, 0);
+            Line geomline = Line.CreateBound(start, end);
+
+            // [6] Assign the height of the wall
+            double height = 15 / 0.3048; // inch to meters
+            double offset = 0;
+
+            // [7] Using Transaction to Create the wall
+            // Command that will edit the document should be included in a transcation
+            Transaction trans = new Transaction(doc, "Create the wall");
+
+            trans.Start();
+            Wall wall = Wall.Create(doc, geomline, wallType.Id, level.Id, height, offset, false, false);
+            trans.Commit();
+
+            return Result.Succeeded;
+        }
+    }
+}
---
+++ ClassLibrary1/ElementFilter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ClassLibrary1
+{
+    class Class3: IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document RevitDoc = commandData.Application.ActiveUIDocument.Document;
+
+            //创建模型元素
+            //RevitDoc.Create.NewFamilyInstance();
+
+            //收集器构造
+            //FilteredElementCollector collector = new FilteredElementCollector(RevitDoc);
+            
+            //FilteredElementCollector collector2 = new FilteredElementCollector(RevitDoc, ICollection<ElementId> elementIds); //文档+ElememtID集合构造
+
+            //FilteredElementCollector collector3 =  new FilteredElementCollector(RevitDoc, ElementId viewId); //文档+视图构造
+
+            // collector + filter => collector.WherePasses(filter)
+
+            FilteredElementCollector collection = new FilteredElementCollector(RevitDoc);
+            ElementFilter filter = new ElementCategoryFilter(BuiltInCategory.OST_StackedWalls);
+            collection.OfClass(typeof(Wall)).WherePasses(filter);
+            ICollection<ElementId> foundIds = collection.ToElementIds();
+
+            //
+            FilteredElementCollector collector3 = new FilteredElementCollector(RevitDoc);
+
+            //查询并遍历文档中所有的Level
+            collector3.WherePasses(new ElementCategoryFilter(BuiltInCategory.OST_Levels))
+                .WhereElementIsNotElementType();
+            foreach(Level level in collector3)
+            {
+                TaskDialog.Show("Level Name", level.Name);
+            }
+
+            // Linq方法
+            FilteredElementCollector collector4 = new FilteredElementCollector(RevitDoc);
+
+            //首先使用一个内建的过滤器来减少后面使用LINQ查询的元素数量
+            collector4.WherePasses(new ElementCategoryFilter(BuiltInCategory.OST_Levels));
+
+
+            //LINQ查询：找到名字为"Level 1"的标高
+            var levelElements = from element in collector4
+                where element.Name == "Level 1"
+                select element;
+            List<Autodesk.Revit.DB.Element> levels = levelElements.ToList<Autodesk.Revit.DB.Element>();
+
+            ElementId level1Id = levels[0].Id;
+
+            //-------------------------------------------------------------------------------------
+            //-------------------------------------------------------------------------------------
+            //-------------------------------------------------------------------------------------
+
+            ///<summary>
+            ///使用ElementCategoryFIlter过滤元素
+            ///</summary>
+            void TestElementCategory
+
+
+
+            return Result.Succeeded;
+        }
+    }
+}
